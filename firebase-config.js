c638be233fd9ec3b02ae35f8a789983abdf8eb15
Wonder239239 <1848213328@qq.com>
2026-04-11 17:50:06 +0800

    configure firebase project

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyDRBmFhBveu5QVIhRutmhONxdFOmgCJClQ",
+  authDomain: "xeasygo-8ef77.firebaseapp.com",
+  projectId: "xeasygo-8ef77",
+  storageBucket: "xeasygo-8ef77.firebasestorage.app",
+  messagingSenderId: "834298387597",
+  appId: "1:834298387597:web:eb96ef7aacd12e198756c4",
+  measurementId: "G-L8KWGXGV44"
+};
